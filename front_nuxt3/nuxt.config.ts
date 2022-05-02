851917c124417ebdcb6ba8abedc3ae1db9305231
TakakiSato <takaki.sato53@gmail.com>
2022-05-03 01:11:11 +0900

    初期設定

--- front_nuxt3/nuxt.config.ts
+++ front_nuxt3/nuxt.config.ts
@@ -3,4 +3,19 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
+  head: {
+    title: 'Nuxt app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  buildModules: [
+    // Simple usage
+    '@nuxtjs/vuetify',
+  ],
 })
