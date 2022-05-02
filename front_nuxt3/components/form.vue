f53343de9bcc517dd6836eaaf61ababfd861a777
TakakiSato <takaki.sato53@gmail.com>
2022-05-03 03:41:36 +0900

    nuxtからデータ取れるようになるまで

---
+++ front_nuxt3/components/form.vue
@@ -0,0 +1,28 @@
+<script setup>
+//http://localhost:4000/app1/graphql?query=query{authorList{name}}
+//http%3A%2F%2Flocalhost%3A4000%2Fapp1%2Fgraphql%3Fquery%3Dquery%7BauthorList%7Bname%7D%7D
+const { data: result } = await useFetch('http://bff:4000/app1/graphql?query=query{authorList{id,name}}');
+
+const author_id = ref(false);
+const author_name = ref(false);
+const book_id = ref(false);
+const book_name = ref(true);
+
+</script>
+
+<template>
+    <div>
+    <h4>author</h4>
+      <input type="checkbox" id="author_id" v-model="author_id">
+      <label for="author_id">author_id: {{ author_id }}</label>
+      <input type="checkbox" id="author_name" v-model="author_name">
+      <label for="author_name">author_name: {{ author_name }}</label>
+    <h4>book</h4>
+      <input type="checkbox" id="book_id" v-model="book_id">
+      <label for="book_id">book_id: {{ book_id }}</label>
+      <input type="checkbox" id="book_name" v-model="book_name">
+      <label for="book_name">book_name: {{ book_name }}</label>
+    <h4>result</h4>
+      {{result}}
+  </div>
+</template>
--- front_nuxt3/nuxt.config.ts
+++ front_nuxt3/nuxt.config.ts
@@ -1,6 +1,6 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
 
   head: {
@@ -9,13 +9,6 @@ export default defineNuxtConfig({
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
   },
-
-  buildModules: [
-    // Simple usage
-    '@nuxtjs/vuetify',
-  ],
 })
+
